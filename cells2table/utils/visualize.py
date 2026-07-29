c9d00163f3f3d0f6eb15002a3a82291feef74b4c
jspast <140563347+jspast@users.noreply.github.com>
2026-07-29 17:23:45 -0300

    Merge eval branch (#4)
    * Update lock file
    
    * Add pytest path to pyproject.toml
    
    * Remove eval files

--- cells2table/utils/visualize.py
+++ cells2table/utils/visualize.py
@@ -3,7 +3,6 @@ import numpy as np
 from numpy.typing import NDArray
 
 from cells2table.datamodels import Table
-from cells2table.models.tasks.detection import DetectionResult
 
 
 def bgr_to_rgb(image: NDArray[np.uint8]) -> NDArray[np.uint8]:
@@ -64,76 +63,3 @@ def visualize_table(
         )
 
     return img
-
-
-def visualize_detections(
-    image: NDArray[np.uint8],
-    detections: list[DetectionResult],
-) -> None:
-    """Detections visualization on top of the image.
-
-    Args:
-        image: A cv2 image of the table.
-        table: The table to draw on top of the image.
-        color: The color of the table overlay.
-        thickness: The thickness of the overlay lines.
-    """
-
-    import matplotlib.cm as cm
-    import matplotlib.colors as mcolors
-    import matplotlib.patches as patches
-    import matplotlib.pyplot as plt
-    from matplotlib.widgets import RangeSlider
-
-    img = bgr_to_rgb(image.copy())
-
-    # Normalize + colormap
-    norm = mcolors.Normalize(vmin=0, vmax=1)
-    cmap = cm.get_cmap("winter")
-
-    # Create figure
-    fig, ax = plt.subplots()
-    plt.subplots_adjust(bottom=0.25)
-    ax.imshow(img)
-    ax.axis("off")
-
-    # Store rectangle artists
-    rects = []
-    texts = []
-
-    for bbox, conf in detections:
-        color = cmap(norm(conf))
-        rect = patches.Rectangle(
-            (bbox[0], bbox[1]),
-            bbox[2] - bbox[0],
-            bbox[3] - bbox[1],
-            linewidth=2,
-            edgecolor=color,
-            facecolor="none",
-        )
-        ax.add_patch(rect)
-        rects.append((rect, conf))
-
-        txt = ax.text(bbox[0], bbox[1] - 5, f"{conf:.2f}", color=color)
-        texts.append((txt, conf))
-
-    # Add range slider
-    slider_ax = plt.axes((0.2, 0.1, 0.6, 0.03))
-    slider = RangeSlider(slider_ax, "Confidence", 0.0, 1.0, valinit=(0.5, 1.0))
-
-    # Update function
-    def update(val):
-        min_conf, max_conf = slider.val
-
-        for rect, conf in rects:
-            rect.set_visible(min_conf <= conf <= max_conf)
-
-        for txt, conf in texts:
-            txt.set_visible(min_conf <= conf <= max_conf)
-
-        fig.canvas.draw_idle()
-
-    update(slider.val)
-    slider.on_changed(update)
-
-    plt.show()
