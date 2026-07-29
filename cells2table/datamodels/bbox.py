7c3dc75bcea7e4a9a0f3543c2d12d654c8ad6da3
jspast <140563347+jspast@users.noreply.github.com>
2026-07-29 17:31:23 -0300

    Fix array type annotation

--- cells2table/datamodels/bbox.py
+++ cells2table/datamodels/bbox.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 
 import numpy as np
-from numpy.typing import ArrayLike, NDArray
+from numpy.typing import NDArray
 
 
 @dataclass
@@ -14,7 +14,7 @@ class BoundingBox:
     b: float
 
     @staticmethod
-    def from_array(bbox: ArrayLike[float]) -> BoundingBox:
+    def from_array(bbox: NDArray[np.float32]) -> BoundingBox:
         return BoundingBox(l=bbox[0], t=bbox[1], r=bbox[2], b=bbox[3])
 
     def as_array(self) -> NDArray[np.float32]:
